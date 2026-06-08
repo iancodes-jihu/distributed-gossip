e5365c58766121100b5e0ed3a34d70b68d232505
ian-iyan <iyaniyannnnnnn@gmail.com>
2026-06-08 20:59:41 +0800

    refactor: rename variables to English, add comments, remove dead

--- src/node.py
+++ src/node.py
@@ -13,10 +13,6 @@ PEERS_A = [5002, 5003]
 PEERS_B = [5004, 5003]
 PEERS_C = [5004, 5002]
 
-
-seen_messages = set()
-
-
 class message:
     def __init__(self, id, origin, payload):
         self.id = id
@@ -39,36 +35,36 @@ class Node:
         self.neighbors = neighbors
         self.port = port
         self.alive = True
-        self.pesan_sudah_dibaca = set()
-        
-    def handle_message(self, psn):
+        self.seen_messages = set()
+        # handle, check if message is new, add to seen set, and forward to peers
+    def handle_message(self, msg):
         if self.alive == False:
             return
         else:
-                if psn.id in self.pesan_sudah_dibaca:
-                    print("Node", self.id, "ignore psn", psn.id, "already seen")
+                if msg.id in self.seen_messages:
+                    print("Node", self.id, "ignore msg", msg.id, "already seen")
                     return
                 else:
-                        self.pesan_sudah_dibaca.add(psn.id)
-                        self.foward_message(psn)
-
-    def send_message(self,  psn, peer_port): 
+                        self.seen_messages.add(msg.id)
+                        self.forward_message(msg)
+        #send new message to connect and send it to all peers
+    def send_message(self,  msg, peer_port): 
             with socket.socket(socket.AF_INET ,socket.SOCK_STREAM)as s:
                 s.connect((HOST, peer_port))
-                s.sendall(json.dumps(psn.to_dict()).encode())
-            print("Node", self.id, "mengirimkan psn", psn.id)
+                s.sendall(json.dumps(msg.to_dict()).encode())
+            print("Node", self.id, "sending msg", msg.id)
             return
     
 
-#handle_message, self, print
-    def foward_message(self, psn):
+#forward-ing message to the peers
+    def forward_message(self, msg):
         
         for peer_port in self.neighbors:
-            self.send_message(psn, peer_port)
-            print("Node", self.id, "forward psn", psn.id, "to", peer_port)
+            self.send_message(msg, peer_port)
+            print("Node", self.id, "forward msg", msg.id, "to", peer_port)
 
 
-    
+#listenin and waiting if there's a new message on loop    
     def listen(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM)as s:
                     s.bind((HOST, self.port))
@@ -76,16 +72,16 @@ class Node:
                     while True:
                         conn, addr = s.accept()
                         with conn: 
-                            print(f"Di koneksikan oleh {addr}")
+                            print(f"connected with {addr}")
                             data = conn.recv(1024)
                             if not data:
                                 break
                             raw = data.decode()
                             data_dict = json.loads(raw)
-                            psn_baru = message(id=str(data_dict["id"]), origin=data_dict["origin"], payload=data_dict["payload"])
-                            self.handle_message(psn_baru)
+                            incoming_msg = message(id=str(data_dict["id"]), origin=data_dict["origin"], payload=data_dict["payload"])
+                            self.handle_message(incoming_msg)
                 
-psn = message (
+msg = message (
     id = str(uuid.uuid4()),
     origin = "A",
     payload = "Hello from A"
@@ -105,5 +101,5 @@ thread_c.start()
 time.sleep(1)
 
 nodeB.alive = False
-nodeA.handle_message(psn)
+nodeA.handle_message(msg)
 
